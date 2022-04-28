b0df059b099bda083ef5f8f5b3d895fd62402f3a
AmrMahmoud <85033377+amrMahmoud19@users.noreply.github.com>
2022-04-28 22:22:03 +0200

    updated arguements passed to cmd

--- Phase1_bash.bat
+++ Phase1_bash.bat
@@ -1,1 +1,1 @@
-python Phase1.py C:\Users\amrmo\Documents\Digital_image_processing-Lane_detection\Project_data\challenge_video.mp4 C:\Users\amrmo\Desktop\ 1 1
+python Phase1.py 1 C:\Users\amrmo\Documents\Digital_image_processing-Lane_detection\Project_data\challenge_video.mp4 C:\Users\amrmo\Desktop\ 1 1
